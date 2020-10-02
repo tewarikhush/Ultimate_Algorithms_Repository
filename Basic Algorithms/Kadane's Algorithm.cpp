3b3b53cc39ae2a579e602bcf969c2bbe68bbf04e
Khushboo <66939185+tewarikhush@users.noreply.github.com>
2020-10-02 11:13:22 +0530

    Update Kadane's Algorithm.cpp

--- Basic Algorithms/Kadane's Algorithm.cpp
+++ Basic Algorithms/Kadane's Algorithm.cpp
@@ -1,29 +1,28 @@
-#include <bits/stdc++.h>
-#define int long long
-#define INF 1000000000000000000
-#define MOD 1000000009;
-#define mid(l, u) ((l+u)/2)
-#define rchild(i) (i*2 + 2)
-#define lchild(i) (i*2 + 1)
+#include<iostream> 
+#include<climits> 
 using namespace std;
 
-signed main(){
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i =0 ;i<n;i++) cin>>a[i];
-    int dp[n];
-    dp[0] = a[0];
-    int ans = a[0];
-    for(int i =1;i<n;i++) ans = max(ans, (dp[i] = a[i] + max(dp[i-1], (int)0)));
-    cout<<ans<<endl;
+int maxSubArraySum(int a[], int size) 
+{ 
+    int max_so_far = INT_MIN, max_ending_here = 0; 
+  
+    for (int i = 0; i < size; i++) 
+    { 
+        max_ending_here = max_ending_here + a[i]; 
+        if (max_so_far < max_ending_here) 
+            max_so_far = max_ending_here; 
+  
+        if (max_ending_here < 0) 
+            max_ending_here = 0; 
+    } 
+    return max_so_far; 
+} 
+int main() 
+{ 
+    int a[] = {-2, -3, 4, -1, -2, 1, 5, -3}; 
+    int n = sizeof(a)/sizeof(a[0]); 
+    int max_sum = maxSubArraySum(a, n); 
+    cout << "Maximum contiguous sum is " << max_sum; 
+    return 0; 
 } 
 
-/*
-Sample Input:
-8
--2 -3 4 -1 -2 1 5 -3
-
-Sample Output:
-7
-*/
